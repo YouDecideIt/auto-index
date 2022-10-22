d7c31268f7c287063a089162970df224704029e5
Zhi Qi <qizhi@pingcap.com>
2022-10-22 11:45:59 +0800

    study ready

---
+++ study/rule.go
@@ -0,0 +1,32 @@
+package study
+
+import (
+	"fmt"
+	"github.com/pingcap/log"
+	"github.com/pingcap/tidb-dashboard/pkg/apiserver/topsql"
+	"go.uber.org/zap"
+)
+
+// Study studies the topsql and return the need-optimize sqls
+func Study() (topsql.SummaryItem, error) {
+	info, err := GetCurrentTopSQLInfo()
+	if err != nil {
+		return topsql.SummaryItem{}, err
+	}
+	hasTiDB := false
+	topOne := topsql.SummaryItem{}
+	for instance, summarys := range info {
+		if instance.InstanceType != "tidb" {
+			continue
+		}
+		hasTiDB = true
+		if topOne.SQLText == "" || topOne.CPUTimeMs < summarys.Data[0].CPUTimeMs {
+			topOne = summarys.Data[0]
+		}
+	}
+	if !hasTiDB {
+		return topsql.SummaryItem{}, fmt.Errorf("no tidb instance")
+	}
+	log.Debug("study succeed", zap.Any("find sql", topOne))
+	return topOne, nil
+}
--- study/study.go
+++ study/study.go
@@ -9,8 +9,8 @@ import (
 	"time"
 )
 
-// Study get the top-sql information from ng-monitor.
-func Study() {
+// GetCurrentTopSQLInfo get the top-sql information from ng-monitor.
+func GetCurrentTopSQLInfo() (map[topsql.InstanceItem]topsql.SummaryResponse, error) {
 	now := time.Now()
 	aHourAgo := now.Add(-time.Hour)
 
@@ -21,22 +21,11 @@ func Study() {
 
 	resp, err := request.GetInstancesWithTime(instanceRequest)
 	if err != nil {
-		return
+		return nil, err
 	}
 	log.Info("get top-sql success", zap.Any("resp", resp))
 
-	//tidbInstance := ""
-	//for _, instance := range resp.Data {
-	//	if instance.InstanceType == "tidb" {
-	//		tidbInstance = instance.Instance
-	//		break
-	//	}
-	//}
-	//
-	//if tidbInstance == "" {
-	//	log.Info("no tidb instance found")
-	//	return
-	//}
+	result := make(map[topsql.InstanceItem]topsql.SummaryResponse)
 
 	for _, instance := range resp.Data {
 		summaryRequest := topsql.GetSummaryRequest{
@@ -52,20 +41,24 @@ func Study() {
 		resp, err := request.GetSummary(summaryRequest)
 		if err != nil {
 			log.Warn("get summary failed", zap.Error(err))
-			return
+			return nil, err
 		}
 		log.Info("get summary succeed", zap.String("instance", instance.Instance))
 		for i, summary := range resp.Data {
-			log.Info("summary", zap.Int("top", i+1),
+			log.Debug("summary", zap.Int("top", i+1),
 				zap.Any("sql text", summary.SQLText),
 				zap.Any("CPUTimeMs", summary.CPUTimeMs),
 				zap.Any("ExecCountPerSec", summary.ExecCountPerSec),
 				zap.Any("DurationPerExecMs", summary.DurationPerExecMs),
 				zap.Any("ScanRecordsPerSec", summary.ScanRecordsPerSec),
 				zap.Any("ScanIndexesPerSec", summary.ScanIndexesPerSec),
+				zap.Any("IsOther", summary.IsOther),
 			)
 		}
 
+		result[instance] = resp
 		//log.Info("get top-sql success", zap.Any("resp", resp))
 	}
+
+	return result, nil
 }
